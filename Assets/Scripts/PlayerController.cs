fb5418759e38e9c4ca36756fd8de6ff309d91e36
JackTesoro <jt1107@scarletmail.rutgers.edu>
2024-01-19 17:03:58 -0500

    rigid bodys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -29,5 +30,13 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
